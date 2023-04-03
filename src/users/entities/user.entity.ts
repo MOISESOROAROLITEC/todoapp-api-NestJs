430c930787f4f4e57bad35c8ae99e08dd16b771b
MOISESOROAROLITEC <moisesoro@arolitec.com>
2023-04-03 17:01:32 +0000

    Generated user module, service and controller

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,26 @@
+import { Timestemp } from "src/timestemps/timestemp";
+import { Todo } from "src/todos/entities/todos.interface";
+import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from "typeorm";
+
+@Entity()
+export class User extends Timestemp {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Column()
+	name: string;
+
+	@Column()
+	lastname: string;
+
+	@Column({
+		unique: true
+	})
+	email: string;
+
+	@Column()
+	password: string;
+
+	@OneToMany(() => Todo, todo => todo.user)
+	todos: Todo[];
+}
