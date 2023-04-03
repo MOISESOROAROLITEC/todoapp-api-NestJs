df4480c26909abf2c0af8c097f3856183f1b8ef0
MOISESOROAROLITEC <moisesoro@arolitec.com>
2023-04-03 10:56:35 +0000

    Add database config and seting in app to connection with him

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TodosModule],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [TodosModule, TypeOrmModule.forRoot({
+		type: 'postgres',
+		host: 'localhost',
+		port: 5432,
+		username: "soro",
+		password: "pass123",
+		database: 'todo',
+		autoLoadEntities: true,
+		synchronize: true
+	})],
+	controllers: [AppController],
+	providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/todos/interfaces/todos.interface.ts
+++ src/todos/interfaces/todos.interface.ts
@@ -1,6 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("todos")
 export class Todo {
+	@PrimaryGeneratedColumn()
 	id: number;
+	@Column()
 	title: string;
+	@Column()
 	description?: string;
+	@Column("json", { nullable: true })
 	done?: boolean;
 }
