97ad51591a255f2a96f588aaf5066b3ebd715d9b
MOISESOROAROLITEC <moisesoro@arolitec.com>
2023-04-05 22:32:50 +0000

    add guard to verify token

--- src/common/guards/api_key/api_key.guard.ts
+++ src/common/guards/api_key/api_key.guard.ts
@@ -1,11 +1,14 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Request } from 'express';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
-  }
+	canActivate(
+		context: ExecutionContext,
+	): boolean | Promise<boolean> | Observable<boolean> {
+		const request = context.switchToHttp().getRequest<Request>()
+		const token = request.header('Authorization')
+		return token === process.env.TOKEN;
+	}
 }
