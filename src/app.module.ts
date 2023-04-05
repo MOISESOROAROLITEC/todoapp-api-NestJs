344ee3aac13d18930ec0d89bd77ae5df2f479229
MOISESOROAROLITEC <moisesoro@arolitec.com>
2023-04-05 17:29:39 +0000

    reformating by prettier

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,34 +9,35 @@ import * as Joi from 'joi';
 import { APP_PIPE } from '@nestjs/core';
 
 @Module({
-	imports: [
-		TypeOrmModule.forRootAsync({
-			useFactory: () => ({
-				type: 'postgres',
-				host: process.env.DB_HOST,
-				port: +process.env.DB_PORT,
-				username: process.env.DB_USERNAME,
-				password: process.env.DB_PASSWORD,
-				database: process.env.DB_NAME,
-				autoLoadEntities: true,
-				synchronize: true,
-			})
-		}),
-		ConfigModule.forRoot({
-			validationSchema: Joi.object({
-				DB_HOST: Joi.required(),
-				DB_PORT: Joi.number().default(5432)
-			})
-		}),
-		TodosModule,
-		UsersModule,
-	],
-	controllers: [AppController],
-	providers: [AppService,
-		{
-			provide: APP_PIPE,
-			useClass: ValidationPipe
-		}
-	],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: +process.env.DB_PORT,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DB_HOST: Joi.required(),
+        DB_PORT: Joi.number().default(5432),
+      }),
+    }),
+    TodosModule,
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
+  ],
 })
-export class AppModule { }
+export class AppModule {}
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -7,42 +7,42 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class TodosService {
-  constructor(
-    @InjectRepository(Todo)
-    private readonly todoRepository: Repository<Todo>,
-  ) {}
+	constructor(
+		@InjectRepository(Todo)
+		private readonly todoRepository: Repository<Todo>,
+	) { }
 
-  async getAll() {
-    return await this.todoRepository.find({
-      relations: ['user'],
-    });
-  }
-  async findOne(id: string) {
-    const todo = await this.todoRepository.findOneBy({ id: +id });
-    if (!todo) {
-      throw new NotFoundException(`Todo #${id} is not found`);
-    }
-    return todo;
-  }
-  createTodo(createTodo: CreateTodoDto) {
-    const todo = this.todoRepository.create(createTodo);
-    return this.todoRepository.save(todo, {});
-  }
-  async update(id: string, updateTodo: UpdateTodoDto): Promise<Todo> {
-    const todo = await this.todoRepository.preload({
-      id: +id,
-      ...updateTodo,
-    });
-    if (!todo) {
-      throw new NotFoundException(`Todo #${id} is not found`);
-    }
-    return this.todoRepository.save(todo);
-  }
-  async delete(id: string): Promise<Todo> {
-    const todo = await this.todoRepository.findOneBy({ id: +id });
-    if (!todo) {
-      throw new NotFoundException('');
-    }
-    return this.todoRepository.remove(todo);
-  }
+	async getAll() {
+		return await this.todoRepository.find({
+			relations: ['user'],
+		});
+	}
+	async findOne(id: string) {
+		const todo = await this.todoRepository.findOneBy({ id: +id });
+		if (!todo) {
+			throw new NotFoundException(`Todo #${id} is not found`);
+		}
+		return todo;
+	}
+	createTodo(createTodo: CreateTodoDto) {
+		const todo = this.todoRepository.create(createTodo);
+		return this.todoRepository.save(todo, {});
+	}
+	async update(id: string, updateTodo: UpdateTodoDto): Promise<Todo> {
+		const todo = await this.todoRepository.preload({
+			id: +id,
+			...updateTodo,
+		});
+		if (!todo) {
+			throw new NotFoundException(`Todo #${id} is not found`);
+		}
+		return this.todoRepository.save(todo);
+	}
+	async delete(id: string): Promise<Todo> {
+		const todo = await this.todoRepository.findOneBy({ id: +id });
+		if (!todo) {
+			throw new NotFoundException('');
+		}
+		return this.todoRepository.remove(todo);
+	}
 }
