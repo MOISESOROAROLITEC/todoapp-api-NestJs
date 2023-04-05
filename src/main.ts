f1fdffe049628a49c899662f00270c48ca9035d3
MOISESOROAROLITEC <moisesoro@arolitec.com>
2023-04-05 18:20:26 +0000

    configure catch httpExceptioFilter

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/http-exception/http-exception';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-    }),
-  );
-  await app.listen(3000);
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			transform: true,
+		}),
+	);
+	app.useGlobalFilters(new HttpExceptionFilter())
+	await app.listen(3000);
 }
 bootstrap();
