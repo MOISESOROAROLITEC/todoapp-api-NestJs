ed87af037e090628a2123b57ec1e7af242493082
MOISESOROAROLITEC <moisesoro@arolitec.com>
2023-04-06 14:26:46 +0000

    add token guard

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { SignupUserDto } from './dto/signup.dto';
 import { LoginUserDto } from './dto/login.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -6,36 +6,38 @@ import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { Todo } from 'src/todos/entities/todos.interface';
 import { CreateTodoDto } from 'src/todos/dto/create-todo.dto';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UsersService {
-  constructor(
-    @InjectRepository(User) private readonly userRepo: Repository<User>,
-    @InjectRepository(Todo) private readonly todoRepo: Repository<Todo>,
-  ) {}
+	constructor(
+		@InjectRepository(User) private readonly userRepo: Repository<User>,
+		@InjectRepository(Todo) private readonly todoRepo: Repository<Todo>,
+	) { }
 
-  createAcount(signupInfo: SignupUserDto) {
-    const user = this.userRepo.create(signupInfo);
-    return this.userRepo.save(user);
-  }
+	createAcount(signupInfo: SignupUserDto) {
+		const user = this.userRepo.create(signupInfo);
+		return this.userRepo.save(user);
+	}
 
-  async connection({ email, password }: LoginUserDto): Promise<User> {
-    const user = await this.userRepo.findOneBy({ email, password });
-    if (!user) {
-      throw new NotFoundException(`user with email : ${email} not found`);
-    }
-    return user;
-  }
-  async createTodo(userId: string, createTodoDto: CreateTodoDto) {
-    const user = await this.userRepo.findOneBy({ id: +userId });
-    if (!user) {
-      throw new NotFoundException(`user with id : ${userId} is not found`);
-    }
-    const todo = this.todoRepo.create({ ...createTodoDto, user });
-    return await this.todoRepo.save(todo);
-  }
-  getUsers() {
-    return this.userRepo.find();
-  }
+	async connection({ email, password }: LoginUserDto): Promise<User> {
+		const user = await this.userRepo.findOneBy({ email, password });
+		if (!user) {
+			throw new NotFoundException(`user with email : ${email} not found`);
+		}
+		return user;
+	}
+	async createTodo(userId: string, createTodoDto: CreateTodoDto) {
+		if (!+userId || +userId < 0) {
+			throw new BadRequestException(`user id must be a positive number`);
+		}
+		const user = await this.userRepo.findOneBy({ id: +userId });
+		if (!user) {
+			throw new NotFoundException(`user with id : ${userId} is not found`);
+		}
+		const todo = this.todoRepo.create({ ...createTodoDto, user });
+		return await this.todoRepo.save(todo);
+	}
+	getUsers() {
+		return this.userRepo.find();
+	}
 }
